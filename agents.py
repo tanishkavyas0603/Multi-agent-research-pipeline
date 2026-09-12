22b775e4bca3a90ebbe04981a1d304f23b545ddc
Tanishka Vyas <tanishka.v634@gmail.com>
2026-09-13 03:22:57 +0530

    Update Groq model to GPT OSS 120B

--- agents.py
+++ agents.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #model setup
-llm= ChatGroq(model="llama-3.3-70b-versatile",
+llm= ChatGroq(model="openai/gpt-oss-120b",
               temperature=0
             )
 
